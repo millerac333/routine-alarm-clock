a37ac9c696c739c5e031dc9a0cae8c2bb447ea11
Ab101200 Ab101200 <millerac333@gmail.com>
2018-10-30 14:21:44 -0500

    added appTask controller and routine controller

---
+++ Controllers/AppTasksController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoutineAlarmClockAPI.Data;
+using RoutineAlarmClockAPI.Models;
+
+namespace RoutineAlarmClockAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppTasksController : ControllerBase
+    {
+        private readonly RoutineAlarmClockAPI_Context _context;
+
+        public AppTasksController(RoutineAlarmClockAPI_Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AppTasks
+        [HttpGet]
+        public IEnumerable<AppTask> GetAppTask()
+        {
+            return _context.AppTask;
+        }
+
+        // GET: api/AppTasks/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAppTask([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var appTask = await _context.AppTask.FindAsync(id);
+
+            if (appTask == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(appTask);
+        }
+
+        // PUT: api/AppTasks/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAppTask([FromRoute] int id, [FromBody] AppTask appTask)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != appTask.AppTaskId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(appTask).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppTaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AppTasks
+        [HttpPost]
+        public async Task<IActionResult> PostAppTask([FromBody] AppTask appTask)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.AppTask.Add(appTask);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAppTask", new { id = appTask.AppTaskId }, appTask);
+        }
+
+        // DELETE: api/AppTasks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppTask([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var appTask = await _context.AppTask.FindAsync(id);
+            if (appTask == null)
+            {
+                return NotFound();
+            }
+
+            _context.AppTask.Remove(appTask);
+            await _context.SaveChangesAsync();
+
+            return Ok(appTask);
+        }
+
+        private bool AppTaskExists(int id)
+        {
+            return _context.AppTask.Any(e => e.AppTaskId == id);
+        }
+    }
+}
---
+++ Controllers/RoutinesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoutineAlarmClockAPI.Data;
+using RoutineAlarmClockAPI.Models;
+
+namespace RoutineAlarmClockAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoutinesController : ControllerBase
+    {
+        private readonly RoutineAlarmClockAPI_Context _context;
+
+        public RoutinesController(RoutineAlarmClockAPI_Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Routines
+        [HttpGet]
+        public IEnumerable<Routine> GetRoutine()
+        {
+            return _context.Routine;
+        }
+
+        // GET: api/Routines/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRoutine([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var routine = await _context.Routine.FindAsync(id);
+
+            if (routine == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(routine);
+        }
+
+        // PUT: api/Routines/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRoutine([FromRoute] int id, [FromBody] Routine routine)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != routine.RoutineId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(routine).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoutineExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Routines
+        [HttpPost]
+        public async Task<IActionResult> PostRoutine([FromBody] Routine routine)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Routine.Add(routine);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRoutine", new { id = routine.RoutineId }, routine);
+        }
+
+        // DELETE: api/Routines/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRoutine([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var routine = await _context.Routine.FindAsync(id);
+            if (routine == null)
+            {
+                return NotFound();
+            }
+
+            _context.Routine.Remove(routine);
+            await _context.SaveChangesAsync();
+
+            return Ok(routine);
+        }
+
+        private bool RoutineExists(int id)
+        {
+            return _context.Routine.Any(e => e.RoutineId == id);
+        }
+    }
+}
