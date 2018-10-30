c951db34fc052c30e003ec498a9f5c2923c6ee79
Ab101200 Ab101200 <millerac333@gmail.com>
2018-10-30 13:44:36 -0500

    uncommented code

--- Models/NewAppUser.cs
+++ Models/NewAppUser.cs
@@ -1,16 +1,16 @@
-﻿//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.ComponentModel.DataAnnotations;
-//using Microsoft.AspNetCore.Identity;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
-//namespace RoutineAlarmClockAPI.Models
-//{
-//    public class NewAppUser
-//    {
-//        [Required]
-//        public string Email { get; set; }
+namespace RoutineAlarmClockAPI.Models
+{
+    public class NewAppUser
+    {
+        [Required]
+        public string Username { get; set; }
 
-//        [Required]
-//        public string Password { get; set; }
-//    }
-//}
+        [Required]
+        public string Password { get; set; }
+    }
+}
