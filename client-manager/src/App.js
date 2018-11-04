8bd11d7fc92c3699350d99ffab2d1fe2a36dcdc3
Ab101200 Ab101200 <millerac333@gmail.com>
2018-11-04 17:14:21 -0600

    begin client

--- client-manager/src/App.js
+++ client-manager/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import ApplicationViews from "./Components/ApplicationViews"
 
-
 class RoutineClock extends Component {
   render() {
     return (
--- client-manager/src/index.js
+++ client-manager/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import RoutineClock from './App';
 import { BrowserRouter as Router } from "react-router-dom"
-
 
 ReactDOM.render(
     <Router>
