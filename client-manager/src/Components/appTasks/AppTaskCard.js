43dcb8f3dcc9b1b4c7d569b76508a39b2cf53e50
Ab101200 Ab101200 <millerac333@gmail.com>
2018-11-05 23:00:19 -0600

    added card component to display user tasks and added modal component for edit task app; need to add functionality

---
+++ client-manager/src/Components/appTasks/AppTaskCard.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+function AppTaskCard(props) {
+  const { classes } = props;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary">
+          Task
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          Task Title
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          5 minutes
+        </Typography>
+        <Typography component="p">
+          Description
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Edit</Button>
+        <Button size="small">Delete</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+AppTaskCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AppTaskCard);
